cd9ecbe99f645ebb7fc0a271663de9d1b474267a
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-09-10 22:07:01 +0600

    problem solving1

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ var price1 = 10;
 var price2 =20;
 totalPrice = price1 + price2;
 console.log(totalPrice);
+
+var momMoney =1000;
+var mangoPrice = 300;
+var orangePrice =200;
+var totalCost= mangoPrice + orangePrice;
+var moneyReturned =momMoney - totalCost;
+console.log(moneyReturned);
