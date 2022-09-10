e3a085265daad62e83ae963e111a40f59436bdd6
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-09-10 22:21:14 +0600

    count resalt

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ var price2 =20;
 totalPrice = price1 + price2;
 console.log(totalPrice);
 
+// problem solving1 
 var momMoney =1000;
 var mangoPrice = 300;
 var orangePrice =200;
 var totalCost= mangoPrice + orangePrice;
 var moneyReturned =momMoney - totalCost;
 console.log(moneyReturned);
+
+
+// problem solving2 
+
+var banglaMarks = 72.25;
+var englishMarks =75;
+var mathMarks =80;
+var biologyMarks =75;
+var totalMarks = banglaMarks + englishMarks +mathMarks + biologyMarks;
+var avaMarks = totalMarks / 4;
+// console.log(avaMarks);
+var avaMarksFinal= avaMarks.toFixed(2);
+// console.log(avaMarksFinal)
+var avaMarksPars= parseInt(avaMarksFinal);
+console.log(avaMarksPars);
